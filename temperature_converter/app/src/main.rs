d7d5294df831c4c4c47ed55fdac7feabdd77f22e
Thomas Le Duc <dev@tleduc.de>
2020-10-24 07:27:05 +0200

    Add interactivity to temperature_converter project

--- temperature_converter/app/src/main.rs
+++ temperature_converter/app/src/main.rs
@@ -1,14 +1,35 @@
+use text_io::read;
+
 fn main() {
-    let is_celsius = true;
-    let temperature = 20.0;
+    let is_celsius = is_input_celsius();
+    let temperature = get_input_temperature();
 
-    let converted_temperature = if is_celsius {
-        convert_celsius_to_fahrenheit(temperature)
+    if is_celsius {
+        let fahrenheit = convert_celsius_to_fahrenheit(temperature);
+        println!("{} Celsius are: {:0.1} Fahrenheit", temperature, fahrenheit);
     } else {
-        convert_fahrenheit_to_celsius(temperature)
+        let celsius = convert_fahrenheit_to_celsius(temperature);
+        println!("{} Fahrenheit are: {:0.1} Celsius", temperature, celsius);
     };
+}
 
-    println!("Your temperature of {} is converted to: {}", temperature, converted_temperature);
+fn is_input_celsius() -> bool {
+    println!("What temperature unit do you want to convert?\nType \"c\" for Celsius or \"f\" for Fahrenheit.");
+    let unit: String = read!();
+
+    if unit == "c" {
+        println!("Chosen unit: Celsius");
+        return true;
+    }
+
+    println!("Chosen unit: Fahrenheit");
+    false
+}
+
+fn get_input_temperature() -> f64 {
+    println!("What temperature do you want to convert?");
+    // todo: handle non float inputs
+    read!()
 }
 
 fn convert_fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
