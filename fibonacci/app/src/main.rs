a221feb3ed7dd5064861b1bc70f247cb0fb54d5c
Thomas Le Duc <dev@tleduc.de>
2020-10-27 08:12:19 +0100

    Add fibonacci project

---
+++ fibonacci/app/src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+
+fn main() {
+    let nth = get_nth_input();
+    let nth_fibonacci = generate_nth_fibonacci(nth);
+
+    println!("The nth (n={}) Fibonacci number is: {}", nth, nth_fibonacci);
+}
+
+fn get_nth_input() -> u32 {
+    println!("Please input which Fibonacci number you want to calculate.");
+    let mut nth = String::new();
+    io::stdin()
+        .read_line(&mut nth)
+        .expect("Failed to read line");
+    match nth.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 0,
+    }
+}
+
+fn generate_nth_fibonacci(nth: u32) -> u32 {
+    if nth == 0 {
+        return 0;
+    } else if nth == 1 {
+        return 1;
+    } else {
+        return generate_nth_fibonacci(nth-1) + generate_nth_fibonacci(nth-2);
+    }
+}
