9096f5b57dc9e32c9731528fc10b4ac575151d37
Thomas Le Duc <dev@tleduc.de>
2020-11-06 09:45:17 +0100

    Add Strings and Hashmaps to collections project

--- collections/app/src/main.rs
+++ collections/app/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 fn main() {
+    // Vectors
     let mut mut_v: Vec<u32> = Vec::new();
     mut_v.push(1);
     mut_v.push(2);
@@ -45,4 +48,88 @@ fn main() {
         SpreadsheetCell::Text(String::from("blue")),
         SpreadsheetCell::Float(10.12),
     ];
+
+    // Strings
+    let mut string = String::new();
+
+    let data = "initial contents";
+    let s_data = data.to_string();
+    let s_data_direct = "initial contents".to_string();
+    let s_from = String::from("initial contents");
+
+    let mut foobar = String::from("foo");
+    foobar.push_str("bar");
+
+    let mut lol = String::from("lo");
+    lol.push('l');
+
+    let hello = String::from("Hello, ");
+    let world = String::from("world!");
+    let hello_world = hello + &world;
+    println!("{}", hello_world);
+
+    let tic = String::from("tic");
+    let tac = String::from("tac");
+    let toe = String::from("toe");
+
+    // let tic_tac_toe = tic + "-" + &tac + "-" + &toe;
+    // println!("{}", tic_tac_toe);
+
+    let tic_tac_toe_format = format!("{}-{}-{}", tic, tac, toe);
+    println!("{}", tic_tac_toe_format);
+
+    let hello = "Здравствуйте";
+    let four_bytes_slice = &hello[0..4];
+    println!("A 0..4 range returns {} from {}.", four_bytes_slice, hello);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    // Hashmaps
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Red"), 50);
+
+    let teams = vec![String::from("Blue"), String::from("Red")];
+    let init_scores = vec![10, 50];
+    let mut scores_collect: HashMap<_, _> =
+        teams.into_iter().zip(init_scores.into_iter()).collect();
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+    let mut color_map = HashMap::new();
+    color_map.insert(field_name, field_value);
+    // println!(
+    //     "field_name {} and field_value {} are not available.",
+    //     field_name, field_value
+    // );
+
+    let score = scores.get("Blue");
+    match score {
+        Some(&team_score) => println!("Blue team's score is: {}", team_score),
+        None => println!("Blue team has no score!"),
+    };
+
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
+
+    scores.insert(String::from("Green"), 10);
+    scores.insert(String::from("Green"), 25);
+    println!("{:?}", scores);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Green")).or_insert(50);
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
