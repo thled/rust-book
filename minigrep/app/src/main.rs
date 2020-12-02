480e3bdeba15e8e79be175c4824507eb021c8027
Thomas Le Duc <dev@tleduc.de>
2020-12-02 07:54:05 +0100

    Add error handling for run

--- minigrep/app/src/main.rs
+++ minigrep/app/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use std::{env, process};
+use std::{error::Error, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,10 +12,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents =
-        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
